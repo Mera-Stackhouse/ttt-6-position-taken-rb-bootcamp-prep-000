c6a112e429ec526e739506f5ad7ad5117082ef3a
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-06 07:05:25 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@
 
 
 def position_taken?(board, index)
-  if board[index] == (("X" )|| ("O" ) )
+  if board[index] == (("X")|| ("O") )
     true
   elsif board[index] == ((" ") ||( "  " )|| (""))
     false
